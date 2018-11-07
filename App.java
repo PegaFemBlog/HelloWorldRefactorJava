c937d67ffc9d8c3d9f58504210bfefc55eee82ea
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-11-07 14:45:47 -0600

    Add the main class to App

---
+++ App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworldrefactor;
+
+/**
+ *
+ * @author bacag
+ */
+public class App {
+    //add the main class here
+    public static void main(String[] args) {
+        HelloWorldRefactor myHelloWorld = new HelloWorldRefactor();
+        myHelloWorld.sayHello();
+    }
+    
+}
